02e28b152f3d7da970fdf8255476eb21fd0a50a5
dineshssdn-867 <66898317+dineshssdn-867@users.noreply.github.com>
2022-09-13 12:18:26 +0530

    Update constants.py

--- videocr/constants.py
+++ videocr/constants.py
@@ -2,6 +2,6 @@ import pathlib
 
 TESSDATA_DIR = pathlib.Path.home() / 'tessdata'
 
-TESSDATA_URL = 'https://github.com/tesseract-ocr/tessdata_fast/raw/master/{}.traineddata'
+TESSDATA_URL = 'https://github.com/tesseract-ocr/tessdata/raw/4.00/eng.traineddata'
 
 TESSDATA_SCRIPT_URL = 'https://github.com/tesseract-ocr/tessdata_best/raw/master/script/{}.traineddata'
